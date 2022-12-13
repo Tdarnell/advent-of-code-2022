246e2336d91bfbf774baa62dcf90b707ec720815
tdarnell <Tdarnell@users.noreply.github.com>
2022-12-13 16:52:08 +0000

    working on part 2

--- python/2022/day8.py
+++ python/2022/day8.py
@@ -1,6 +1,8 @@
 import sys
+
 sys.path.insert(0, ".")
 import utils
+
 
 def loop_left(grid_row, start_col=0):
     # This will loop through the grid from left to right, optionally from a given index
@@ -9,20 +11,19 @@ def loop_left(grid_row, start_col=0):
     # with 0 being the smallest and 9 being the tallest
     grid_row = grid_row.copy()
     max_seen = grid_row[start_col]
-    trees_seen = [grid_row[start_col]]
     # Set all of the trees that are before the start_col to None
     grid_row[:start_col] = [None] * start_col
     # Loop from left to right
-    for i,tree in enumerate(grid_row):
+    for i, tree in enumerate(grid_row):
         if i <= start_col:
             continue
         if tree > max_seen:
             max_seen = tree
-            trees_seen.append(tree)
         elif i != start_col:
             grid_row[i] = None
-    return trees_seen, grid_row
-    
+    return grid_row
+
+
 def loop_right(grid_row, start_col=0):
     """
     This will loop through the grid from right to left, optionally from a given index
@@ -34,8 +35,9 @@ def loop_right(grid_row, start_col=0):
     if start_col != 0:
         start_col = len(grid_row) - start_col - 1
     grid_row = grid_row[::-1]
-    trees_seen, left_row = loop_left(grid_row, start_col=start_col)
-    return trees_seen, left_row[::-1]
+    left_row = loop_left(grid_row, start_col=start_col)
+    return left_row[::-1]
+
 
 def create_rows(grid_text):
     # This will create a list of lists of the grid rows
@@ -44,6 +46,7 @@ def create_rows(grid_text):
     for line in lines:
         rows.append([int(x) for x in line])
     return rows
+
 
 def create_columns(rows: list):
     # This will create a list of lists of the grid columns
@@ -61,6 +64,7 @@ def create_columns(rows: list):
         columns.append([rows[rw][col] for rw in range(dims[1])])
     return columns
 
+
 def part1(input_file):
     # This will return the answer to part 1
     # of the puzzle
@@ -71,30 +75,56 @@ def part1(input_file):
         columns = create_columns(rows)
         visible_rows = []
         for r in rows:
-            trees_seen_l, grid_row_l = loop_left(r)
-            trees_seen_r, grid_row_r = loop_right(r)
-            visible_rows.append([x if x is not None else y for x,y in zip(grid_row_l, grid_row_r)])
+            grid_row_l = loop_left(r)
+            grid_row_r = loop_right(r)
+            visible_rows.append(
+                [x if x is not None else y for x, y in zip(grid_row_l, grid_row_r)]
+            )
         visible_columns = []
         for c in columns:
-            trees_seen_l, grid_row_l = loop_left(c)
-            trees_seen_r, grid_row_r = loop_right(c)
-            visible_columns.append([x if x is not None else y for x,y in zip(grid_row_l, grid_row_r)])
+            grid_row_l = loop_left(c)
+            grid_row_r = loop_right(c)
+            visible_columns.append(
+                [x if x is not None else y for x, y in zip(grid_row_l, grid_row_r)]
+            )
         # Now we transpose our visible columns again using the get_columns function
         visible_grid = []
         transposed_columns = create_columns(visible_columns)
-        for i,row in enumerate(visible_rows):
-            visible_grid.append([x if x is not None else y for x,y in zip(row, transposed_columns[i])])
+        for i, row in enumerate(visible_rows):
+            visible_grid.append(
+                [x if x is not None else y for x, y in zip(row, transposed_columns[i])]
+            )
         # now count the number of trees that are not None
         trees = sum(sum(x is not None for x in row) for row in visible_grid)
         print(f"Part 1: The number of trees visible from outside the grid is {trees}")
-        return visible_grid
-            
-    
+        return visible_grid, rows, columns
+
+
+def get_scenic_score(rows, columns, start_col, start_row):
+    """
+    This will return the scenic score for the given row and column
+    It does this by looping through the rows and columns and
+    working out how many trees can be seen left, right, up and down
+    from that given point. It then multiplies these numbers together.
+    """
+    # get the row and column that we are starting from
+    row = rows[start_row]
+    columns = columns[start_col]
+    left_row = loop_left(row, start_col=start_col)
+    right_row = loop_right(row, start_col=start_col)
+    left_col = loop_left(columns, start_col=start_row)
+    right_col = loop_right(columns, start_col=start_row)
+    score = []
+    for arr in [left_row, right_row, left_col, right_col]:
+        score.append(sum(x is not None for x in arr))
+    return score[0] * score[1] * score[2] * score[3]
+
 if __name__ == "__main__":
-    visible_trees = part1(utils.get_input(8, 2022))
+    visible_trees, rows, columns = part1(utils.get_input(8, 2022))
     # save the visible trees to a file
     with open("outputs/2022/day8_part1_visible_trees.txt", "w") as f:
         for row in visible_trees:
             f.write("".join([str(x) if x is not None else "." for x in row]) + "\n")
-    
-    
+    # Now we need to find the best place to build a house
+    score = get_scenic_score(rows, columns, 10, 10)
+    print(f"Part 2: The best place to build a house is at 10, 10 with a score of {score}")
