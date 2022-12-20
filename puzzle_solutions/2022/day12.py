8d0cda20e1f23eb40f25387e675d5497d190c17b
tdarnell <Tdarnell@users.noreply.github.com>
2022-12-20 23:55:21 +0000

    Day 12 - Working solution for example data, but not for input

--- puzzle_solutions/2022/day12.py
+++ puzzle_solutions/2022/day12.py
@@ -64,6 +64,7 @@ def get_adjacent_values(chars: list, pos: tuple, verbose: bool = False):
         return None, None, None, None
     if verbose:
         print(f"Dimensions of char list are {dims}")
+    pos_value = chars[pos[0]][pos[1]]
     if pos[0] == 0:
         if verbose:
             print("Top row")
@@ -96,7 +97,18 @@ def get_adjacent_values(chars: list, pos: tuple, verbose: bool = False):
         right = (pos[0], pos[1] + 1)
     if verbose:
         print(f"Adjacent values are {up}, {down}, {left}, {right}")
-    return up, down, left, right
+    adj_values = [up, down, left, right]
+    for i, pos in enumerate(adj_values):
+        if pos is not None:
+            if verbose:
+                print(f"Value at {pos} is {chars[pos[0]][pos[1]]}")
+            val = chars[pos[0]][pos[1]]
+            # If the val is not within +/- 1 of the pos_value, set the pos to None
+            if abs(values[val] - values[pos_value]) > 1:
+                if verbose:
+                    print(f"Value at {pos} is not within +/- 1 of {pos_value}")
+                adj_values[i] = None
+    return adj_values
 
 
 def build_graph(chars: list, verbose: bool = False):
@@ -117,19 +129,107 @@ def build_graph(chars: list, verbose: bool = False):
     for i in range(dims[0]):
         for j in range(dims[1]):
             # graph[(i, j)] = get_adjacent_values(chars, (i, j), verbose=verbose)
-            graph[(i, j)] = {pos: get_values(chars, pos) for pos in get_adjacent_values(chars, (i, j), verbose=verbose) if pos is not None}
+            graph[(i, j)] = {
+                pos: get_values(chars, pos)
+                for pos in get_adjacent_values(chars, (i, j), verbose=verbose)
+                if pos is not None
+            }
     return graph
 
-# I understand that I need to solve the djikstra algorithm to solve this problem
-# I have not yet done this, but I have written the code to build the graph
+
+## Dijkstra's algorithm
+# https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+# https://www.youtube.com/watch?v=gdmfOwyQlcI
+# Excellent Learning material for this: https://www.redblobgames.com/pathfinding/a-star/introduction.html
+# Better implementation available: https://www.redblobgames.com/pathfinding/a-star/implementation.html#python-astar
+# Their implementation however depends on a priority queue, for my understanding I will implement it without
+# as I am learning this algorithm for the first time.
+# Code for this function written with assistance from OpenAI ChatGPT
+def dijkstra(graph: list, start: tuple, end: tuple):
+    if not isinstance(graph, dict) or not isinstance(start, tuple) or not isinstance(end, tuple):
+        raise Exception("Graph must be a dictionary and start and end must be tuples.")
+    if start not in graph or end not in graph:
+        raise Exception("Start or end node not in graph.")
+    # create a dictionary to store the cost (distance) to each node
+    # set the cost to infinity for all nodes
+    cost_to_node = {node: float("inf") for node in graph}
+    # set the cost to the start node to 0
+    cost_to_node[start] = 0
+    # create a dictionary to store the parent of each node in the shortest path
+    parent = {node: None for node in graph}
+    # create a set to store the visited nodes
+    visited = set()
+
+    # loop until the end node is visited
+    while end not in visited:
+        # find the node with the smallest distance that has not been visited
+        smallest = float("inf")
+        current_node = None
+        for node, cost in cost_to_node.items():
+            if cost < smallest and node not in visited:
+                smallest = cost
+                current_node = node
+        # if there are no unvisited nodes left, we are done
+        if current_node is None:
+            break
+        # mark the current node as visited
+        visited.add(current_node)
+        # consider all of the current node's neighbors
+        for neighbor, cost in graph[current_node].items():
+            # calculate the cost to the neighbor through the current node
+            new_cost = cost_to_node[current_node] + cost
+            # if the new cost is less than the current cost to the neighbor, update the cost and set the current node as the neighbor's parent
+            if new_cost < cost_to_node[neighbor]:
+                cost_to_node[neighbor] = new_cost
+                parent[neighbor] = current_node
+    print(f"Cost to node {end} is {cost_to_node[end]}")
+    print(f"Parent of node {end} is {parent[end]}")
+    # print(cost_to_node)
+    # print(f"Visited nodes are {visited}")
+    # create a list to store the shortest path
+    shortest_path = []
+    # set the current node to the end node
+    current_node = end
+    # loop through the parent dictionary to build the shortest path
+    while current_node is not None:
+        shortest_path.append(current_node)
+        current_node = parent[current_node]
+    # reverse the list to get the shortest path in the correct order
+    shortest_path.reverse()
+    # return the shortest path and the cost
+    return shortest_path, cost_to_node[end]
+
 
 if __name__ == "__main__":
-    chars = format_input(verbose=True)
-    pos_E = get_position(verbose=True)
-    pos_S = get_position(char="S", verbose=True)
-    char_vals = get_values(chars, pos=None, verbose=True)
-    print(char_vals)
-    u, d, l, r = get_adjacent_values(chars, pos_E, verbose=True)
-    u, d, l, r = get_adjacent_values(chars, pos_S, verbose=True)
-    graph = build_graph(chars, verbose=True)
-    print(f"Graph is {graph}")
+    # chars = format_input(verbose=True)
+    # pos_E = get_position(verbose=True)
+    # pos_S = get_position(char="S", verbose=True)
+    # char_vals = get_values(chars, pos=None, verbose=True)
+    # print(char_vals)
+    # u, d, l, r = get_adjacent_values(chars, pos_E, verbose=True)
+    # u, d, l, r = get_adjacent_values(chars, pos_S, verbose=True)
+    # graph = build_graph(chars, verbose=True)
+    # graph_values_E = graph[pos_E]
+    # print(f"Graph is {graph}, graph_values_E is {graph_values_E}")
+    # shortest_path, cost = dijkstra(graph, pos_S, pos_E)
+    # print(
+    #     f"Shortest path is {shortest_path} with a length of {len(shortest_path) - 1}, it has a cost of {cost}"
+    # )
+    ## Part 1
+    input_data = utils.get_input(year=2022, day=12)
+    with open(input_data) as f:
+        input_txt = f.read().strip()
+        chars = format_input(input_txt=input_txt, verbose=False)
+        pos_E = get_position(chars, verbose=False)
+        pos_S = get_position(chars, char="S", verbose=False)
+        print(f"pos_E is {pos_E}, pos_S is {pos_S}")
+        graph = build_graph(chars, verbose=False)
+        print(f"Graph of size {len(graph)} built")
+        print(f"The graph values for pos_S are {graph[pos_S]} and for pos_E are {graph[pos_E]}")
+        shortest_path, cost = dijkstra(graph, start=pos_S, end=pos_E)
+        # Worked for the example, but not for the actual input, so I need to debug this in the morning
+        # I think the problem is that the start is not (0, 0) as it was in the example
+        print(f"Shortest path is {shortest_path} with a cost of {cost}")
+        print(f"Part 1: {len(shortest_path) - 1}")
+        
+        
